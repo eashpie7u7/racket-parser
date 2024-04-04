791b0f50b56db483d1c0ee49ec350f2513cf3c50
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2024-04-04 02:05:04 -0600

    WIP Parser

--- src/lexer.rkt
+++ src/lexer.rkt
@@ -29,84 +29,80 @@
  (SEMICOLON COMMA DOT LEFT_BRACE RIGHT_BRACE LEFT_BRACKET RIGHT_BRACKET LEFT_PAREN RIGHT_PAREN EOF))
 
 (define lex
-  ;WHITESPACE
-  (lexer [#\space (lex input-port)]
-         [#\newline (lex input-port)]
-         ;COMMENTS
-         [(:seq "//" (:* (:~ #\newline)) #\newline) (lex input-port)]
-         ;KEYWORDS
-         ["if" (token-IF)]
-         ["else" (token-ELSE)]
-         ["for" (token-FOR)]
-         ["while" (token-WHILE)]
-         ["break" (token-BREAK)]
-         ["continue" (token-CONTINUE)]
-         ["return" (token-RETURN)]
-         ["function" (token-FUNCTION)]
-         ["var" (token-VAR)]
-         ["let" (token-LET)]
-         ["const" (token-CONST)]
-         ["class" (token-CLASS)]
-         ;OPERATORS
-         ["+" (token-PLUS)]
-         ["-" (token-MINUS)]
-         ["*" (token-MULTIPLY)]
-         ["/" (token-DIVIDE)]
-         ["=" (token-ASSIGNMENT)]
-         ["==" (token-EQUAL)]
-         ["!=" (token-NOT_EQUAL)]
-         ["<" (token-LESS_THAN)]
-         [">" (token-GREATER_THAN)]
-         ["<=" (token-LESS_THAN_OR_EQUAL)]
-         [">=" (token-GREATER_THAN_OR_EQUAL)]
-         ["&&" (token-AND)]
-         ["||" (token-OR)]
-         ;PUNCTUATION
-         [";" (token-SEMICOLON)]
-         ["(" (token-LEFT_PAREN)]
-         [")" (token-RIGHT_PAREN)]
-         ["[" (token-LEFT_BRACKET)]
-         ["]" (token-RIGHT_BRACKET)]
-         ["{" (token-LEFT_BRACE)]
-         ["}" (token-RIGHT_BRACE)]
-         ["." (token-DOT)]
-         ["," (token-COMMA)]
-         ;STRING LITERAL
-         [(:seq #\" (:* (:~ #\")) #\") (token-STRING lexeme)]
-         ;NUMBER LITERAL
-         [(:seq (:+ numeric) (:? (:seq #\. (:* numeric)))) (token-NUMBER (string->number lexeme))]
-         ;NULL LITERAL
-         ["null" (token-NULL lexeme)]
-         ;TRUE LITERAL
-         ["true" (token-BOOLEAN lexeme)]
-         ;FALSE LITERAL
-         ["false" (token-BOOLEAN lexeme)]
-         ;IDENTIFIERS
-         [(:seq (:or alphabetic #\_) (:* (union alphabetic numeric #\_))) (token-IDENTIFIER lexeme)]
-         ;EOF
-         [(eof) (token-EOF)]
-         ;any
-         [any-char (lex input-port)]))
 
-(define (tokenize input)
-  (let loop ([tokens '()])
-    (let ([next-token (lex input)])
-      (if (eq? next-token `EOF)
-          (reverse (cons (list 'eof "eof") tokens))
-          (loop (cons next-token tokens))))))
+  (lexer-src-pos
+   ;COMMENTS
+   [(:seq "//" (:* (:~ #\newline)) #\newline) (lex input-port)]
+   ;KEYWORDS
+   ["if" (token-IF)]
+   ["else" (token-ELSE)]
+   ["for" (token-FOR)]
+   ["while" (token-WHILE)]
+   ["break" (token-BREAK)]
+   ["continue" (token-CONTINUE)]
+   ["return" (token-RETURN)]
+   ["function" (token-FUNCTION)]
+   ["var" (token-VAR)]
+   ["let" (token-LET)]
+   ["const" (token-CONST)]
+   ["class" (token-CLASS)]
+   ;OPERATORS
+   ["+" (token-PLUS)]
+   ["-" (token-MINUS)]
+   ["*" (token-MULTIPLY)]
+   ["/" (token-DIVIDE)]
+   ["=" (token-ASSIGNMENT)]
+   ["==" (token-EQUAL)]
+   ["!=" (token-NOT_EQUAL)]
+   ["<" (token-LESS_THAN)]
+   [">" (token-GREATER_THAN)]
+   ["<=" (token-LESS_THAN_OR_EQUAL)]
+   [">=" (token-GREATER_THAN_OR_EQUAL)]
+   ["&&" (token-AND)]
+   ["||" (token-OR)]
+   ;PUNCTUATION
+   [";" (token-SEMICOLON)]
+   ["(" (token-LEFT_PAREN)]
+   [")" (token-RIGHT_PAREN)]
+   ["[" (token-LEFT_BRACKET)]
+   ["]" (token-RIGHT_BRACKET)]
+   ["{" (token-LEFT_BRACE)]
+   ["}" (token-RIGHT_BRACE)]
+   ["." (token-DOT)]
+   ["," (token-COMMA)]
+   ;STRING LITERAL
+   [(:seq #\" (:* (:~ #\")) #\") (token-STRING lexeme)]
+   ;NUMBER LITERAL
+   [(:seq (:+ numeric) (:? (:seq #\. (:* numeric)))) (token-NUMBER (string->number lexeme))]
+   ;NULL LITERAL
+   ["null" (token-NULL lexeme)]
+   ;TRUE LITERAL
+   ["true" (token-BOOLEAN lexeme)]
+   ;FALSE LITERAL
+   ["false" (token-BOOLEAN lexeme)]
+   ;IDENTIFIERS
+   [(:seq (:or alphabetic #\_) (:* (union alphabetic numeric #\_))) (token-IDENTIFIER lexeme)]
+   ;whitespace
+   [whitespace (token-COMMA)]
+   ;EOF
+   [(eof) (token-EOF)]))
 
-(define (tokenize-file path)
-  (tokenize (open-input-file path)))
+(define src-code (open-input-file "src/test.js"))
+(port-count-lines! src-code) ;enable lines and cols nums
 
-(tokenize-file "src/test.js")
+(lex src-code)
+(lex src-code)
+(lex src-code)
+(lex src-code)
+(lex src-code)
+(lex src-code)
 
 ;exports
-(provide tokenize-file)
-
 (provide literal-tokens)
 (provide identifier-tokens)
 (provide keyword-tokens)
 (provide expression-operator-tokens)
 (provide term-operator-tokens)
 (provide punctuation-tokens)
+
 (provide lex)
---
+++ src/parser.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require "lexer.rkt")
+
+(require parser-tools/yacc)
+
+(define error-handler
+  (lambda (tok-ok? tok-name tok-value start-pos end-pos)
+    (printf "\n\n\nError: Invalid token detected: ~a, Value: ~a\n\n\n\n" tok-name tok-value)
+    ;(printf "Start position: ~a, End position: ~a\n" start-pos end-pos)
+    ))
+
+(define the-parser
+  (parser [start expr]
+          [end EOF]
+          [src-pos]
+          [error error-handler]
+          [tokens
+           literal-tokens
+           identifier-tokens
+           keyword-tokens
+           expression-operator-tokens
+           term-operator-tokens
+           punctuation-tokens]
+          [grammar [expr [(NUMBER COMMA NUMBER) (+ $1 $3)]]]))
+
+(define src-code (open-input-file "src/test.js"))
+(port-count-lines! src-code) ;enable lines and cols nums
+
+(define result (the-parser (λ () (lex src-code))))
+
+(display result)
