42986c6420de986059e43a2ec3f4fcb9c49d5a80
Eashley Martínez <A01663894@tec.mx>
2024-06-07 14:11:59 -0600

    Declaración, asignación e inicialización de variables

--- src/parser.rkt
+++ src/parser.rkt
@@ -11,7 +11,6 @@
     (printf "\n\n\nError: Invalid token detected: ~a, Value: ~a\n\n\n\n" tok-name tok-value)
     ;(printf "Start position: ~a, End position: ~a\n" start-pos end-pos)
     ))
-
 (define the-parser
   (parser [start c-program]
           [end EOF]
@@ -41,7 +40,8 @@
            [statement [(declaration SEMICOLON) $1]
                       [(expr SEMICOLON) $1]]
            
-           [declaration [(type-token IDENTIFIER) (list 'declare $1 $2)]]
+           [declaration [(type-token IDENTIFIER) (list 'declare $1 $2)]
+                       [(type-token IDENTIFIER ASSIGNMENT expr) (list 'declare-init $1 $2 $4)]]
            
            [type-token [(INT) 'int]
                        [(CHAR) 'char]
@@ -49,13 +49,15 @@
                        [(DOUBLE) 'double]
                        [(VOID) 'void]]
            
-           [expr [(NUMBER PLUS NUMBER) (+ $1 $3)]
+           [expr [(IDENTIFIER ASSIGNMENT expr) (list 'assign $1 $3)]
+                 [(NUMBER PLUS NUMBER) (+ $1 $3)]
                  [(NUMBER MINUS NUMBER) (- $1 $3)]
                  [(NUMBER MULTIPLY NUMBER) (* $1 $3)]
                  [(NUMBER DIVIDE NUMBER) (/ $1 $3)]
                  [(NUMBER INCREMENT) (increment $1)]
                  [(NUMBER DECREMENT) (decrement $1)]
                  [(IDENTIFIER) $1]
+                 [(NUMBER) $1]
                  [(LEFT_PAREN expr RIGHT_PAREN) $2]]
            ]))
 
