1be6efe47524aa401e19042b12edad6a131b4f0d
Eashley Martínez <A01663894@tec.mx>
2024-06-07 17:50:33 -0600

    Test en c

--- src/test.c
+++ src/test.c
@@ -1,1 +1,12 @@
-5 + 5 
+int main() {
+    printf("Hello, World!");
+    int x = 10;
+    for (int i = 0; i < 5; i++) {
+        x = x + i;
+    }
+    if (x > 15) {
+        printf("x is greater than 15");
+    } else {
+        printf("x is not greater than 15");
+    }
+}
