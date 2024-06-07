3cc22fe929d978eccfd533fe3776287d77fd3279
Eashley Martínez <A01663894@tec.mx>
2024-06-07 17:36:19 -0600

    Creación de archivo HTML con CSS

--- src/parser.rkt
+++ src/parser.rkt
@@ -3,7 +3,8 @@
 (require "lexer.rkt")
 
 (require parser-tools/yacc)
-(define (increment x) 
+;; Increment and decrement functions
+(define (increment x)
   (cond [(number? x) (+ x 1)]
         [(symbol? x) (list 'post-increment x)]))
 
@@ -11,13 +12,13 @@
   (cond [(number? x) (- x 1)]
         [(symbol? x) (list 'post-decrement x)]))
 
-
+;; Error handler function
 (define error-handler
   (lambda (tok-ok? tok-name tok-value start-pos end-pos)
-    (printf "Sintaxis incorrecta" )
-    (printf "\n\n\nError: Invalid token detected: ~a, Value: ~a\n\n\n\n" tok-name tok-value)
-    ;(printf "Start position: ~a, End position: ~a\n" start-pos end-pos)
-    ))
+    (printf "Sintaxis incorrecta")
+    (printf "\n\n\nError: Invalid token detected: ~a, Value: ~a\n\n\n\n" tok-name tok-value)))
+
+;; Parser definition
 (define the-parser
   (parser [start c-program]
           [end EOF]
@@ -33,61 +34,47 @@
            punctuation-tokens]
           [grammar 
            [c-program [(main-function) $1]]
-           
            [main-function [(INT IDENTIFIER LEFT_PAREN RIGHT_PAREN block)
-                          (if (equal? $2 "main")
-                              (list 'main-function $5)
-                              (error "Expected 'main' function"))]]
-           
+                           (if (equal? $2 "main")
+                               (list 'main-function $5)
+                               (error "Expected 'main' function"))]]
            [block [(LEFT_BRACE statements RIGHT_BRACE) $2]]
-           
            [statements [(statement statements) (cons $1 $2)]
                        [() '()]]
-           
            [statement [(declaration SEMICOLON) $1]
                       [(expr SEMICOLON) $1]
                       [(if-statement) $1]
                       [(for-statement) $1]
                       [(function-call SEMICOLON) $1]]
-           
            [if-statement [(IF LEFT_PAREN expr RIGHT_PAREN block) 
-                         (list 'if $3 $5)]
-                        [(IF LEFT_PAREN expr RIGHT_PAREN block ELSE block) 
-                         (list 'if-else $3 $5 $7)]
-                        [(IF LEFT_PAREN expr RIGHT_PAREN statement) 
-                         (list 'if $3 $5)]
-                        [(IF LEFT_PAREN expr RIGHT_PAREN statement ELSE statement) 
-                         (list 'if-else $3 $5 $7)]]
-           
+                          (list 'if $3 $5)]
+                         [(IF LEFT_PAREN expr RIGHT_PAREN block ELSE block) 
+                          (list 'if-else $3 $5 $7)]
+                         [(IF LEFT_PAREN expr RIGHT_PAREN statement) 
+                          (list 'if $3 $5)]
+                         [(IF LEFT_PAREN expr RIGHT_PAREN statement ELSE statement) 
+                          (list 'if-else $3 $5 $7)]]
            [for-statement [(FOR LEFT_PAREN for-init SEMICOLON for-cond SEMICOLON for-incr RIGHT_PAREN block)
-                         (list 'for $3 $5 $7 $9)]
-                        [(FOR LEFT_PAREN for-init SEMICOLON for-cond SEMICOLON for-incr RIGHT_PAREN statement)
-                         (list 'for $3 $5 $7 $9)]]
-           
+                           (list 'for $3 $5 $7 $9)]
+                          [(FOR LEFT_PAREN for-init SEMICOLON for-cond SEMICOLON for-incr RIGHT_PAREN statement)
+                           (list 'for $3 $5 $7 $9)]]
            [for-init [(declaration) $1]
                      [(expr) $1]
                      [() #f]]
-           
            [for-cond [(expr) $1]
                      [() #t]]
-           
            [for-incr [(expr) $1]
                      [() #f]]
-           
            [function-call [(IDENTIFIER LEFT_PAREN args RIGHT_PAREN) (list 'call $1 $3)]]
-           
            [args [(expr) (list $1)]
                  [() '()]]
-           
            [declaration [(type-token IDENTIFIER) (list 'declare $1 $2)]
-                       [(type-token IDENTIFIER ASSIGNMENT expr) (list 'declare-init $1 $2 $4)]]
-           
+                        [(type-token IDENTIFIER ASSIGNMENT expr) (list 'declare-init $1 $2 $4)]]
            [type-token [(INT) 'int]
                        [(CHAR) 'char]
                        [(FLOAT) 'float]
                        [(DOUBLE) 'double]
                        [(VOID) 'void]]
-           
            [expr [(IDENTIFIER ASSIGNMENT expr) (list 'assign $1 $3)]
                  [(expr PLUS expr) (list '+ $1 $3)]
                  [(expr MINUS expr) (list '- $1 $3)]
@@ -104,11 +91,60 @@
                  [(STRING) $1]]
            ]))
 
+;; Function to generate highlighted HTML
+(define (surround1 s1 c)
+  (string-append "<" c ">" s1))
 
+(define (surround2 s1 c)
+  (string-append "<" c ">" s1 "</" c ">"))
 
-(define src-code (open-input-file "test.c"))
-(port-count-lines! src-code) ;enable lines and cols nums
+(define (surround3 s1 c)
+  (string-append s1 "</" c ">"))
 
-(define result (the-parser (λ () (lex src-code))))
+(define (sep str)
+  (regexp-split #rx"(?<=\\]|\\[|(\r\n)|[*+-/()<>{}=;: ])|(?=\\]|\\[|(\r\n)|[*+-/()<>{}=;: ])" str))
 
-(display result)
+(define (surroundRegexp s1)
+  (cond
+    [(regexp-match #rx"[0-9]+" s1) (set! s1 (surround2 s1 "num"))]
+    [(regexp-match #rx"\\." s1) (set! s1 (surround2 s1 "num"))]
+    [(regexp-match #rx"[><*=/+-]" s1) (set! s1 (surround2 s1 "op"))]
+    [(regexp-match #rx"^else$|^if$" s1) (set! s1 (surround2 s1 "cond"))]
+    [(regexp-match #rx"^for$|^while$" s1) (set! s1 (surround2 s1 "loop"))]
+    [(regexp-match #rx"\".*\"" s1) (set! s1 (surround2 s1 "string"))]
+    [(regexp-match #rx"(?<=\")(?=.)" s1) (set! s1 (surround1 s1 "string"))]
+    [(regexp-match #rx"(?<=.)(?=\")" s1) (set! s1 (surround3 s1 "string"))]
+    [(regexp-match #rx"(?<=')(?=.)" s1) (set! s1 (surround1 s1 "string"))]
+    [(regexp-match #rx"(?<=.)(?=')" s1) (set! s1 (surround3 s1 "string"))]
+    [(regexp-match #rx"'.*'" s1) (set! s1 (surround2 s1 "string"))]
+    [(regexp-match #rx"\\]|\\[|[(]|[)]|[{]|[}]" s1) (set! s1 (surround2 s1 "par"))]
+    [(regexp-match #rx"[,]" s1) (set! s1 (surround2 s1 "punct"))]
+    [(regexp-match #rx" " s1) (set! s1 "&nbsp ")]
+    [(regexp-match #rx"(\r\n)" s1) (set! s1 "<br>")]
+    )s1)
+
+(define (resaltar2 x)
+  (set! x (sep x))
+  (set! x (map (lambda (lst) (surroundRegexp lst)) x))
+  (set! x (string-join x ""))
+  (set! x (string-append " <html> <head> <link rel=\"stylesheet\" href=\"estilo.css\"> </head> <body> " x "</body> </html> "))
+  x)
+
+(define (res ruta string)
+  (call-with-output-file ruta
+    (lambda (port)
+      (display string port))
+    #:exists 'replace))
+
+(define (resultado x)
+  (res "resaltado.html" (resaltar2 x)))
+
+;; Main function to parse the source code and generate highlighted HTML
+(define (main)
+  (define src-code (open-input-file "test.c"))
+  (port-count-lines! src-code)
+  (define result (the-parser (λ () (lex src-code))))
+  (resultado (file->string "test.c"))
+  (display result))
+
+(main)
